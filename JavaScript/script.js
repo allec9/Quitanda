ed6804c22eae275c6639a53dc293573d3cf0c52f
Allec C. L. <50810927+allec9@users.noreply.github.com>
2024-11-26 20:55:35 -0300

    Add files via upload

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -12,15 +12,56 @@ const inicioPrograma = Number(prompt(`
     ____________________________
     `))
 
-switch (fruta) {
+    function comprarMaracuja() {
+        var resultado = 0
+        alert("Preço do maracuja é R$1,30 a unidade, a partir de 12 unidades o preço é R$1,00")
+        var qtdMaracuja = Number(prompt("Quantos Maracujás você deseja comprar?"))
+        /* var precoUm = 1.30
+        var calculoMaracuja = qtdMaracuja  */
+        if (qtdMaracuja >= 12) {
+            resultado = qtdMaracuja * 1
+            /* precoUm = 1 */
+            alert(`Você comprou ${qtdMaracuja} e saiu por ${resultado.toLocaleString('pt-br', 
+                {
+                    style: 'currency',
+                    currency: 'BRL',
+                }
+            )}`)
+        }else {
+            resultado = qtdMaracuja * 1.30
+            alert(`Você comprou ${qtdMaracuja} maracujás e saiu por ${resultado.toLocaleString('pt-br', 
+                {
+                    style: 'currency',
+                    currency: 'BRL',
+                }
+            )}`)}
+    }
+    function comprarTomate() {
+        resultado = 0
+        var qtdTomate = Number(prompt("Quantos Tomates você deseja comprar?"))
+        if (qtdTomate >= 2) {
+            var resultado = qtdTomate * 6.99
+            alert(`Você comprou ${qtdTomate} e saiu por ${resultado}`)
+        }else {
+            resultado = qtdTomate * 8.55
+            alert(`Você comprou ${qtdTomate} Tomate e saiu por ${resultado}`)
+        }
+        
+    }
+    function comprarCupuacu() {
+        alert("Você tem 3 opções de Manga: [1] Redondo R$5,49   [2] Mamorama R$10,00   [3] Mamau R$2,99")
+    }
+switch (inicioPrograma) {
     case 1:
-        alert("Você escolheu = Maracuja! XD")
+        comprarMaracuja()
         break;
     case 2:
-        alert("Você escolheu = Tomate! XD")
+        comprarTomate()
+        /* O KG do Tomate é de R$8,55, a partir de 2KG sai por R$6,99 */
+        alert("")
         break;
     case 3:
-        alert("Você escolheu = Cupuacu! XD")
+        comprarCupuacu()
         break;
     case 4:
         alert("Você escolheu = Pitaya! XD")
